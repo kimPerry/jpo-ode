91e88cb7ae9036411f3ea31535679e72da3cc92d
585158 <sandesh@terpmail.umd.edu>
2017-04-28 10:41:56 -0400

    ODE-308 Code clean up

---
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/vsdm/VsdmReceiver.java
@@ -0,0 +1,17 @@
+package us.dot.its.jpo.ode.vsdm;
+
+import java.net.DatagramSocket;
+
+public class VsdmReceiver implements Runnable{
+	private DatagramSocket socket = null;
+	
+	public VsdmReceiver(){
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
